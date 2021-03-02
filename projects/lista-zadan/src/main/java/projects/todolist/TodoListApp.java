93a397e2f37fd1d97eb7683b40d32a39f63da16a
drikszajd <d_rikszajd@o2.pl>
2021-03-02 19:23:47 +0100

    praca w grupie cd

--- projects/lista-zadan/src/main/java/projects/todolist/TodoListApp.java
+++ projects/lista-zadan/src/main/java/projects/todolist/TodoListApp.java
@@ -1,11 +1,25 @@
 package projects.todolist;
 
+import projects.todolist.config.DbUtil;
+import projects.todolist.model.Task;
 import projects.todolist.option.TodoListOption;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TodoListApp {
 
     public static void main(String[] args) {
+        DbUtil.getSession();
+
+        Task task1 = new Task("Bieganie", LocalDateTime.of(2021,3,3,8,00).plusMinutes(30), LocalDateTime.now());
+        Task task2 = new Task("Kurs", LocalDateTime.of(2021,3,2,16,30).plusMinutes(00), LocalDateTime.now());
+        List<Task> tasks = new ArrayList<>();
+
         TodoListOption todoListOption = new TodoListOption();
         todoListOption.run();
+
+
     }
 }
